a4f2b113d827cbbfbec7f6ee54d9accd5debc687
rtclub11140 <saeree_club@hotmail.co.th>
2026-07-19 00:26:53 +0700

    feat: delete project

--- apps/api/src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { HousesModule } from './houses/houses.module';
-import { IncidentsModule } from './incidents/incidents.module';
-import { PrismaModule } from './prisma/prisma.module';
-
-@Module({
-  imports: [PrismaModule, HousesModule, IncidentsModule],
-})
-export class AppModule {}
